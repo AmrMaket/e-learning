8f9a9a32e2eb8ba1ddb8cf166b28cac82b883e93
Amr Maket <amrmaket89@gmail.com>
2023-08-12 23:01:53 +0300

    Client <> adding Announcments

--- e-learning-client/src/App.js
+++ e-learning-client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from '../src/pages/Dashboard/index';
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
