fc0e8ac3c6b53e4e8f55bb6547bffb96e3080778
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-08-12 15:57:00 +0300

    Client <> Modal Mesasge for Parent design

--- e-learning-client/src/components/ParentSingleChat/index.jsx
+++ e-learning-client/src/components/ParentSingleChat/index.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
-import './styles.css';
+import React, { useState } from "react";
+import "./styles.css";
+import MessageModal from "../MessageModal";
 
 function SingleChat({ item }) {
-  const link = "/" + item.id;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  //hadi: i want to send the message later
+  const [message, setMessage] = useState("");
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const sendMessage = (message) => {
+    console.log("Sent message:", message);
+  };
 
   return (
-    <div className='person-item'>
-      <span className='person-name'>{item.name}</span>
-      <a href={link} className='chat-link'>
-        <span className='chat-icon'>&#128231;</span>
-      </a>
+    <div className="person-item">
+      <span className="person-name">{item.name}</span>
+      <div className="chat-link" onClick={openModal}>
+        <span className="chat-icon">&#128231;</span>
+      </div>
+      {isModalOpen && (
+        <MessageModal closeModal={closeModal} sendMessage={sendMessage} />
+      )}
     </div>
   );
 }
