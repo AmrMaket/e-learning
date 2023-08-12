9716ad263e7640150136286187227936fa2e678a
Amr Maket <amrmaket89@gmail.com>
2023-08-12 13:19:57 +0300

    Client <> Adding ButtonAdd Component

--- e-learning-client/src/App.js
+++ e-learning-client/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ButtonAdd from './components/ButtonAdd';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ButtonAdd />} />
+          {/* <Route path="/landing" element={<Landing />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/singleView" element={<SingleView />} /> */}
+        </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ e-learning-client/src/pages/Course_Creation/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Course_Creation = () => {
+    return ( 
+        <div>
+            
+        </div>
+     );
+}
+ 
+export default Course_Creation;
