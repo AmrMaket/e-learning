7285f831236b8326fbcedeece2e20a310351562c
Amr Maket <amrmaket89@gmail.com>
2023-08-13 18:49:44 +0300

    Client <> Fixing Teacher Pages

--- e-learning-client/src/pages/teacher/CourseCreation/index.jsx
+++ e-learning-client/src/pages/teacher/CourseCreation/index.jsx
@@ -1,9 +1,26 @@
-const CourseCreation = () => {
-    return (
-        <div className="course-creation-main-container">
-            This is the CourseCreation page
+import React, { useState } from 'react';
+import './style.css';
+import Popups from '../../../components/Popups/index';
+
+const CreateCoursePage = () => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const handleCircleClick = () => {
+    setIsPopupOpen(!isPopupOpen);
+    const circleIcon = document.querySelector('.circle-icon');
+    circleIcon.classList.toggle('open');
+  };
+
+  return (
+    <div className="create-course-page">
+      <div className={`circle ${isPopupOpen ? 'active' : ''}`} onClick={handleCircleClick}>
+        <div className='circle'>
+          <span className={`circle-icon ${isPopupOpen ? 'open' : ''}`}>&#8853;</span>
         </div>
-    );
-}
- 
-export default CourseCreation;
+      </div>
+      {isPopupOpen && <Popups />}
+    </div>
+  );
+};
+
+export default CreateCoursePage;
