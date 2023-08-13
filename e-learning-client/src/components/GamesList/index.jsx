5abff6829cda13eec6af9f40ef5959462f44d770
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-08-13 13:36:35 +0300

    Client <> Added Games List Component

---
+++ e-learning-client/src/components/GamesList/index.jsx
@@ -0,0 +1,24 @@
+import '../../base.css'
+import './styles.css'
+
+const GamesList = (props) => {
+
+    const games = props.games;
+
+    return (
+        <div className="games-main-container flex flex-col align-center">
+            <h2 className='padding-s'>Games</h2>
+            <div className="games-list flex flex-col gap-l">
+                {games.map((game) => (
+                    <div className="game-preview flex flex-col padding-s justify-center align-center gap-s" key={game.id}>
+                        <h3>{game.title}</h3>
+                        <div>Game link: {game.link}</div>
+                    </div>
+                ))}
+            </div>
+        </div>
+        
+    );
+}
+ 
+export default GamesList;
