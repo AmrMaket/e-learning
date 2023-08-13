e2aef6866a9b87a847183d0a5164ba4f5fab9a73
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-08-13 09:34:12 +0300

    Client <> Testing Calendly npm

--- e-learning-client/src/pages/parent/ParentTeacherConference/index.jsx
+++ e-learning-client/src/pages/parent/ParentTeacherConference/index.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import './styles.css'
+import React, { useState, useEffect } from "react";
+import { PopupButton } from "react-calendly";
+import axios from "axios";
 
 function Conference() {
+  const [teacher, setTeacher] = useState({
+    name: "",
+    url: "https://calendly.com/cheaitohadi97/30min",
+  });
+
+  useEffect(() => {
+    axios
+      .get("api here to get name and url")
+      .then((response) => {
+        const teacherData = response.data;
+        setTeacher(teacherData);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
-    <div>index</div>
-  )
+    <div className="App">
+      <div className="container">
+        <div className="teacher-name">{teacher.name}</div>
+        <PopupButton
+          url={teacher.url}
+          rootElement={document.getElementById("root")}
+          text="Book a meeting!"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Conference
+export default Conference;
