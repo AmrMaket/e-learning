8b06d1fcfbd6369ade0b8ee1d75b9812602e302b
Amr Maket <amrmaket89@gmail.com>
2023-08-13 12:48:02 +0300

    Client <> Adding Lectures

--- e-learning-client/src/App.js
+++ e-learning-client/src/App.js
@@ -5,7 +5,7 @@ import TeacherNavbar from './components/TeacherNavbar';
 import AttendanceTracking from './pages/teacher/AttendanceTracking';
 import CollaborationTools from './pages/teacher/CollaborationTools';
 import Communication from './pages/teacher/Communication';
-import CourseForm from './pages/CourseCreation/index';
+import CourseForm from './components/LecturesOnToggle/index';
 import GradingAndFeedback from './pages/teacher/GradingAndFeedback';
 
 //Parent-related imports
