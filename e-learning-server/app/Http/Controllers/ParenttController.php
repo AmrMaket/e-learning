3eb42b3e419d332650fd1bfec89a6cbd5abfe185
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-08-14 18:14:17 +0300

    Server <> Added Basic Parent Controller Functions

--- e-learning-server/app/Http/Controllers/ParenttController.php
+++ e-learning-server/app/Http/Controllers/ParenttController.php
@@ -6,5 +6,66 @@ use Illuminate\Http\Request;
 
 class ParenttController extends Controller
 {
-    //
+    public function index()
+    {
+        $parent = Parentt::all();
+        return response()->json($parent);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make( $request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string',
+            'role_id' => 'required|numeric',
+            'child_id' => 'required|numeric',
+        ]);
+
+        if ( $validator->fails() ) {
+            return response()->json($validator->errors(), 500);
+        }
+
+        $parent = Parentt::create($request->all());
+        return response()->json($parent, 200);
+    }
+
+    public function show(Parentt $parent)
+    {
+        return response()->json($parent, 200);
+    }
+
+    public function edit(Parentt $parent)
+    {
+        //
+    }
+
+    public function update(Request $request, Parentt $parent)
+    {
+        $validator = Validator::make( $request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string',
+            'role_id' => 'required|numeric',
+            'child_id' => 'required|numeric',
+        ]);
+
+        if ( $validator->fails() ) {
+            return response()->json($validator->errors(), 500);
+        }
+
+        $parent = Parentt::create($request->all());
+        return response()->json($parent, 200);
+    }
+
+    public function destroy(Parentt $parent)
+    {
+        $parent->delete();
+        return response()->json('',200);
+    }
 }
