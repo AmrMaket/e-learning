c71b707c578548557d6c3cf7449311d9df764521
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-08-14 13:05:38 +0300

    Client <> Login with api

--- e-learning-client/src/pages/Login/index.jsx
+++ e-learning-client/src/pages/Login/index.jsx
@@ -1,15 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import Input from "../../components/Input";
 import "./styles.css";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  // const navigate = useNavigate();
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleDataChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post("http://127.0.0.1:8000/api/login", credentials);    
+      if (response.status === 200) {
+        const responseData = response?.data;
+        if (responseData) {
+          localStorage.setItem("token", responseData.authorization.token);
+          // navigate("/landing");
+          console.log("logged");
+        }
+      }
+    } catch (error) {
+      console.log("Can't log in: " + error);
+    }
+  };
+  
+
   return (
     <div className="container">
       <div className="login-box">
         <h2 className="login-heading">Login</h2>
-        <Input label="Email" placeholder="Enter your email" name="email" type="text" />
-        <Input label="Password" placeholder="Enter your password" name="password" type="password" />
-        <button className="login-btn">Login</button>
+        <Input
+          label="Email"
+          placeholder="Enter your email"
+          value={credentials.email}
+          onChange={handleDataChange}
+          name="email"
+          type="text"
+        />
+        <Input
+          label="Password"
+          placeholder="Enter your password"
+          value={credentials.password}
+          onChange={handleDataChange}
+          name="password"
+          type="password"
+        />
+        <button className="login-btn" onClick={handleLogin}>
+          Login
+        </button>
       </div>
     </div>
   );
