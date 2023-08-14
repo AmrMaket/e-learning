1e58251f92cba6bda80da4d649c3921d106d834a
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-08-14 22:23:39 +0300

    Server <> Added Function to Get Quizzes Grades

--- e-learning-server/routes/api.php
+++ e-learning-server/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ParenttController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::get('child_quiz_grades',[ParenttController::class, 'getQuizGrades'] );
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
