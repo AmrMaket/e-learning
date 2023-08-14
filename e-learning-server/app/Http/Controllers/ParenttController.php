687e8cc05fb081438ee10898d6b80bfad3718116
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-08-15 00:14:18 +0300

    Server <> Updated GetQuizGrades Function

--- e-learning-server/app/Http/Controllers/ParenttController.php
+++ e-learning-server/app/Http/Controllers/ParenttController.php
@@ -73,58 +73,43 @@ class ParenttController extends Controller
         return response()->json('',200);
     }
 
-    // public function getQuizGrades()
-    // {
-    //     try{// $user = Auth::user();
-    //     $user_id = 2;
-    //     $childId = User::select('child_id')
-    //     ->where('id', '=', $user_id)
-    //     ->get();
-    //     $childId = json_decode($childId, true)[0]['child_id'];
-
-    //     $childName = User::select('name')
-    //     // ->where('id', '=', $user->id)
-    //     ->where('id', '=', $childId)
-    //     ->get();
-    //     $childName = json_decode($childName, true)[0]['name'];
-
-    //     $results = DB::table('student_quizzes')
-    //     ->join('quizzes', 'student_quizzes.quiz_id', '=', 'quizzes.id')
-    //     ->where('student_quizzes.student_id', $childId)
-    //     ->select('student_quizzes.grade', 'quizzes.name')
-    //     ->get();
-
-    //     return response()->json([
-    //         'status' => 'success',
-    //         'child' => $childName,
-    //         'results' => $results,
-    //     ]);
-    // } catch (\Exception $e) {
-    //     Log::error($e->getMessage());
-    
-    //     return response()->json([
-    //         'status' => 'error',
-    //         'message' => 'An error occurred',
-    //     ], 500);
-    // }
-    // }
     public function getQuizGrades()
-        {
-            try{// $user = Auth::user();
-            $parentId = 2;
-            
-            $results = DB::table('users')
-            -> join('student_quizzes', 'users.child_id', '=', 'student_quizzes.student_id')
-            -> join('quizzes', 'student_quizzes.quiz_id', '=', 'quizzes.id')
-            -> join('courses', 'quizzes.course_id', '=', 'courses.id')
-            -> where('users.id', $parentId)
-            -> select('courses.name', 'student_quizzes.grade', 'quizzes.grade_overall')
-            ->get();
+    {
+        try{
+        // $parentId = Auth::user();
+        $parentId = 2;
+        
+        $results = DB::table('users')
+        -> join('student_quizzes', 'users.child_id', '=', 'student_quizzes.student_id')
+        -> join('quizzes', 'student_quizzes.quiz_id', '=', 'quizzes.id')
+        -> join('courses', 'quizzes.course_id', '=', 'courses.id')
+        -> where('users.id', $parentId)
+        ->select(
+            'courses.name as course_name', 
+            'student_quizzes.grade as student_grade', 
+            'quizzes.grade_overall as quiz_grade_overall'
+        )
+        ->get();
+
+        $averagedResults = [];
+        $groupedResults = collect($results)->groupBy('course_name');
     
-            return response()->json([
-                'status' => 'success',
-                'results' => $results,
-            ]);
+        foreach ($groupedResults as $courseName => $group) {
+            $totalStudentGrade = $group->sum('student_grade');
+            $numQuizzes = $group->count();
+            $averageStudentGrade = $numQuizzes > 0 ? $totalStudentGrade / $numQuizzes : 0;
+
+            $averagedResults[] = [
+                'course_name' => $courseName,
+                'avg_quiz_grade' => $averageStudentGrade,
+                'quiz_grade_overall' => $group->first()->quiz_grade_overall,
+            ];
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'results' => $averagedResults,
+        ]);
         } catch (\Exception $e) {
             Log::error($e->getMessage());
         
@@ -133,5 +118,5 @@ class ParenttController extends Controller
                 'message' => 'An error occurred',
             ], 500);
         }
-        }
+    }
 }
