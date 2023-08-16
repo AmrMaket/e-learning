516987ede69208520732e32fea2c636abe40fd2b
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-08-16 06:56:50 +0300

    Client <> Added fetchCourses API request

--- e-learning-client/src/pages/student/CourseEnrollment/index.jsx
+++ e-learning-client/src/pages/student/CourseEnrollment/index.jsx
@@ -1,16 +1,48 @@
 import CourseEnrollmentList from "../../../components/CourseEnrollmentList";
-import { useState } from 'react';
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import '../../../base.css'
 import './styles.css'
 
 const CourseEnrollment = () => {
 
-    const [courses, setCourses] = useState ([
-        {title: 'UI/UX', enrollment: 'Enrolled', capacity: '30', id: 1},
-        {title: 'English', enrollment: 'Enrolled', capacity: '25', id: 2},
-        {title: 'Biology', enrollment: 'Not Enrolled', capacity: '20', id: 3},
-        {title: 'Database', enrollment: 'Enrolled', capacity: '10', id: 4},
-    ]);
+    // const [courses, setCourses] = useState ([
+    //     {title: 'UI/UX', enrollment: 'Enrolled', capacity: '30', id: 1},
+    //     {title: 'English', enrollment: 'Enrolled', capacity: '25', id: 2},
+    //     {title: 'Biology', enrollment: 'Not Enrolled', capacity: '20', id: 3},
+    //     {title: 'Database', enrollment: 'Enrolled', capacity: '10', id: 4},
+    // ]);
+
+    const [courses, setCourses] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect( () => {
+        fetchCourses();
+    }, []);
+
+    const fetchCourses = async () => {
+        try{
+            const response = await axios.get('http://127.0.0.1:8000/api/courses_available');
+
+            const transformedArray = response.data.courses.map(data => ({
+                title: data.name,
+                enrollment: (data.capacity - data.remaining_capacity) > 0 ? 'Enrolled' : 'Not Enrolled',
+                capacity: data.capacity.toString(),
+                id: data.course_id
+              }));
+
+            setCourses(transformedArray);
+            setLoading(false);
+        }
+        catch (error){
+            console.error('Error fetching contacts', error);
+            setLoading(false);
+        }
+    };
+
+    if(loading){
+        return <p>Please wait while we gather your contacts</p>;
+    }
 
     return (
         <div className="course-enrollment-main-container flex flex-col padding-l">
