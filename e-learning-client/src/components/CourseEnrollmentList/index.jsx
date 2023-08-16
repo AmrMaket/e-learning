cd9a8f8ef0acc9eef59b61bc29e6ef6eba113e54
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-08-16 07:09:32 +0300

    Client <> Linked EnrollCourse to the API

--- e-learning-client/src/components/CourseEnrollmentList/index.jsx
+++ e-learning-client/src/components/CourseEnrollmentList/index.jsx
@@ -1,15 +1,27 @@
 import '../../base.css'
 import './styles.css'
 import { useState } from 'react';
+import axios from 'axios';
 
 const CoursesList = (props) => {
 
     const courses = props.courses;
 
     const [enrolledCourses, setEnrolledCourses] = useState([]);
-
+    const [disabledButtons, setDisabledButtons] = useState([]);
+    
     const enrollCourse = (courseId) => {
-            setEnrolledCourses([...enrolledCourses, courseId]);
+            // setEnrolledCourses([...enrolledCourses, courseId]);
+            setDisabledButtons([...disabledButtons, courseId]);
+            try{
+                axios.post('http://127.0.0.1:8000/api/student_enrollment', {
+                    user_id: 1,
+                    course_id: courseId
+                });
+            }
+            catch(error) {
+                console.error('Error:', error);
+            };
     };
 
     return (
@@ -20,9 +32,10 @@ const CoursesList = (props) => {
                     <div>Enrolled status: {course.enrollment}</div>
                     <div>Course capacity: {course.capacity}</div>
                     <button 
-                    className={`enrolled-course-card-button white-text padding-s ${enrolledCourses.includes(course.id) ? 'disabled-button' : ''}`}
-                    disabled={enrolledCourses.includes(course.id)}
-                    onClick={() => enrollCourse(course.id)}>
+                        className={`enrolled-course-card-button white-text padding-s ${enrolledCourses.includes(course.id) ? 'disabled-button' : ''}`}
+                        disabled={enrolledCourses.includes(course.id) || disabledButtons.includes(course.id)}
+                        onClick={() => enrollCourse(course.id)}
+                    >
                         {enrolledCourses.includes(course.id) ? 'Enrolled' : 'Enroll'}
                     </button>
                 </div>
